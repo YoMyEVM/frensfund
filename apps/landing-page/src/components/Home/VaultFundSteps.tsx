e88574a09fa1820382a72a37226fe79e5ef9d1c2
YoMyEVM <yo.myevm@gmail.com>
2024-11-09 05:14:59 +0000

    .

---
+++ apps/landing-page/src/components/Home/VaultFundSteps.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const VaultFundSteps = () => {
+  return (
+    <div className="flex justify-center items-center p-6">
+      <img 
+        src="/vault-fund-steps.png" 
+        alt="Vault Fund Steps" 
+        className="w-1/2 h-auto rounded-lg shadow-md"
+      />
+    </div>
+  );
+};
+
+export default VaultFundSteps;
--- apps/landing-page/src/pages/index.tsx
+++ apps/landing-page/src/pages/index.tsx
@@ -1,12 +1,14 @@
-import { HomeContent } from '@components/Home/HomeContent'
-import { HomeHeader } from '@components/Home/HomeHeader'
-import { Layout } from '@components/Layout'
+import { HomeContent } from '@components/Home/HomeContent';
+import { HomeHeader } from '@components/Home/HomeHeader';
+import { Layout } from '@components/Layout';
+import VaultFundSteps from '@components/Home/VaultFundSteps';
 
 export default function HomePage() {
   return (
     <Layout>
-      <HomeHeader className='mt-10' />
-      <HomeContent className='py-12' />
+      <HomeHeader className="mt-10" />
+      <VaultFundSteps />
+      <HomeContent className="py-12" />
     </Layout>
-  )
+  );
 }
